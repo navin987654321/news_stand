a74aadd85ad93a9abf2f8b424b4d3f5307d353c6
Sujit Jayaraj <sujitjayaraj@live.com>
2021-04-08 16:09:18 -0400

    News fetched from twitter

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,34 +1,39 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
 import requests
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from newsapi import NewsApiClient
+import twint
 # Create your views here.
 
 def home(request):
     page = request.GET.get('page', 1)
     search = request.GET.get('search', None)
-
+    sia = SentimentIntensityAnalyzer()
     if search is None:
-        url = "https://newsapi.org/v2/top-headlines?page={}&apiKey={}".format(page, settings.NEWSAPI_KEY)
+        query = 'United States'
     else:
-        url = "https://newsapi.org/v2/top-headlines?page={}&apiKey={}&q={}".format(page, settings.NEWSAPI_KEY, search)
-    
-    r = requests.get(url=url)
-    data = r.json()
-    if data["status"] != "ok":
-        return HttpResponse("<h1>Could not fetch your news feed, please try again later!")
-    data = data["articles"]
+        query = search
+    c = twint.Config()
+    c.Search = query
+    c.Limit = 1000
+    c.Lang = 'en'
+    c.Pandas = True
+    c.Hide_output = True
+    twint.run.Search(c)
+    data = twint.output.panda.Tweets_df[['id', 'username', 'photos', 'tweet', 'date']].to_dict(orient='index')
     context = {
         "success": True,
         "data": [],
         "search": search
     }
-    for i in data:
+    for i in data.values():
         context["data"].append({
-            "title": i["title"],
-            "description": "" if i["description"] is None else i["description"],
-            "url": i["url"],
-            "image": i["urlToImage"],
-            "publishedat": i["publishedAt"]
+            "username": i['username'],
+            "image": "https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-superJumbo-v4.jpg" if len(i["photos"])==0 else i["photos"][0],
+            "content": i['tweet'],
+            "publishedat": i['date'],
+            "polarity": sia.polarity_scores("" if i['tweet'] is None else i['tweet'])
         })
     return render(request, 'home.html', context=context)
