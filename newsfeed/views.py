06efa8ea9d78aac3bd8c543b470886a6e8d73b7f
Sujit Jayaraj <sujitjayaraj@live.com>
2021-04-09 18:01:58 -0400

    Seperate feeds for twitter and NewsAPI data

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -6,9 +6,9 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import newsfeed.tweets as tweets
 # Create your views here.
 
+sia = SentimentIntensityAnalyzer()
 def home(request):
     search = request.GET.get('search', None)
-    sia = SentimentIntensityAnalyzer()
     if search is None:
         query = 'United States'
     else:
@@ -30,3 +30,34 @@ def home(request):
             "polarity": sia.polarity_scores(i['cleaned_tweet']),
         })
     return render(request, 'home.html', context=context)
+
+def newsapi(request):
+    page = request.GET.get('page', 1)
+    search = request.GET.get('search', None)
+
+    if search is None:
+        url = "https://newsapi.org/v2/top-headlines?country={}&apiKey={}".format("us", settings.NEWSAPI_KEY)
+    else:
+        url = "https://newsapi.org/v2/everything?q={}&apiKey={}".format(search, settings.NEWSAPI_KEY)
+    r = requests.get(url=url)
+
+    data = r.json()
+    if data["status"] != "ok":
+        return HttpResponse("<h1>Request Failed</h1>")
+    data = data["articles"]
+    context = {
+        "success": True,
+        "data": [],
+        "search": search
+    }
+    for i in data:
+        context["data"].append({
+            "title": i["title"],
+            "content": "" if i["description"] is None else i["description"],
+            "url": i["url"],
+            "image": "" if i["urlToImage"] is None else i["urlToImage"],
+            "publishedat": i["publishedAt"],
+            "polarity": sia.polarity_scores("" if i["description"] is None else i["description"])
+        })
+    
+    return render(request, 'newsapi.html', context=context)
